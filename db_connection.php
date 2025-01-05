bd8c6bfd880c135694f1a6ca86a2322dfaa83dba
Sireen Al Zoughbi <sireenalzoughbi1@example.com>
2025-01-05 18:48:52 +0200

    Initial commit

---
+++ db_connection.php
@@ -0,0 +1,6 @@
+<?php
+$con=mysqli_connect("sql207.infinityfree.com","if0_38040798","yBT8Y1w6vN6h ","if0_38040798_flight");
+if(mysqli_connect_errno()){
+    echo"Failed to connect to MySQL:".mysqli_connect_error();
+}
+?>
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'register.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: AccountUs(),
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+    ),
+  );
+}
